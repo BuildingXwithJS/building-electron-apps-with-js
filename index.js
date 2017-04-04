17db33a18c90a2cfb955ec090def06a43e6e4e39
Tim Ermilov <yamalight@gmail.com>
2017-04-04 17:15:44 +0200

    Add basic React and webpack setup

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+// npm packages
+const url = require('url');
+const path = require('path');
 const electron = require('electron');
+// our packages
+const startServer = require('./serve');
+
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
-
-const path = require('path');
-const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
+  // start webpack server
+  startServer();
+
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -19,13 +25,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true,
-    })
-  );
+  mainWindow.loadURL('http://localhost:3000/index.html');
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!');
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// render on page
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+  </div>,
+  document.getElementById('app')
+);
