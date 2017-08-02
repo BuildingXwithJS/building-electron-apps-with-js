d98543eff11fa19601754a650b559f1a00b165ee
Tim Ermilov <yamalight@gmail.com>
2017-08-02 19:39:46 +0200

    Use uglify to minify the built code

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import {minify} from 'uglify-es';
 
 export default {
   entry: 'src/index.js',
   format: 'cjs',
   dest: 'dist/app.min.js',
-  plugins: [babel({exclude: 'node_modules/**'})],
+  plugins: [babel({exclude: 'node_modules/**'}), uglify({}, minify)],
   external: [
     'big-integer',
     'cheerio',
